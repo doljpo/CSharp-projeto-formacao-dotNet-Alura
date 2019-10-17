b6ce384172fe3ebdf13118ddd1471fb98056b98b
David Oliveira <dol.jpo@hotmail.com>
2019-10-17 08:46:14 -0300

    > Definição da classe Livro movida para modelo.cs; > Correção do comportamento do botão 'Home' no _Layout.cshtml; > Correção no comportamento do botão 'Continuar comprando' na tela de Cadastro.

--- CasaDoCodigo/Models/modelo.cs
+++ CasaDoCodigo/Models/modelo.cs
@@ -140,4 +140,13 @@ namespace CasaDoCodigo.Models
         public int Id { get; set; }
         public string Nome { get; set; }
     }
+
+    public class Livro
+    {
+        public string Codigo { get; set; }
+        public string Nome { get; set; }
+        public string Categoria { get; set; }
+        public string Subcategoria { get; set; }
+        public decimal Preco { get; set; }
+    }
 }
--- CasaDoCodigo/Repositories/ProdutoRepository.cs
+++ CasaDoCodigo/Repositories/ProdutoRepository.cs
@@ -51,13 +51,4 @@ namespace CasaDoCodigo.Repositories
             await contexto.SaveChangesAsync();
         }
     }
-
-    public class Livro
-    {
-        public string Codigo { get; set; }
-        public string Nome { get; set; }
-        public string Categoria { get; set; }
-        public string Subcategoria { get; set; }
-        public decimal Preco { get; set; }
-    }
 }
